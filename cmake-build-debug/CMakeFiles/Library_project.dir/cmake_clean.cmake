3767fb03e6294824c435ec1d9b8ec1ab9e1cbef5
LRusinov <lubomir202020@gmail.com>
2022-07-17 16:28:17 +0300

    fixes

---
+++ cmake-build-debug/CMakeFiles/Library_project.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Library_project.dir/Admin.cpp.obj"
+  "CMakeFiles/Library_project.dir/Admin.cpp.obj.d"
+  "CMakeFiles/Library_project.dir/Article.cpp.obj"
+  "CMakeFiles/Library_project.dir/Article.cpp.obj.d"
+  "CMakeFiles/Library_project.dir/Book.cpp.obj"
+  "CMakeFiles/Library_project.dir/Book.cpp.obj.d"
+  "CMakeFiles/Library_project.dir/Date.cpp.obj"
+  "CMakeFiles/Library_project.dir/Date.cpp.obj.d"
+  "CMakeFiles/Library_project.dir/Library.cpp.obj"
+  "CMakeFiles/Library_project.dir/Library.cpp.obj.d"
+  "CMakeFiles/Library_project.dir/LibraryItem.cpp.obj"
+  "CMakeFiles/Library_project.dir/LibraryItem.cpp.obj.d"
+  "CMakeFiles/Library_project.dir/Reader.cpp.obj"
+  "CMakeFiles/Library_project.dir/Reader.cpp.obj.d"
+  "CMakeFiles/Library_project.dir/Series.cpp.obj"
+  "CMakeFiles/Library_project.dir/Series.cpp.obj.d"
+  "CMakeFiles/Library_project.dir/main.cpp.obj"
+  "CMakeFiles/Library_project.dir/main.cpp.obj.d"
+  "Library_project.exe"
+  "Library_project.exe.manifest"
+  "Library_project.pdb"
+  "libLibrary_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Library_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
